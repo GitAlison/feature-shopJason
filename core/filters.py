09a4fc4ca3944fc79b1fd5120911268445276e8b
Alison <alysson.k888@gmail.com>
2019-11-30 18:35:14 -0300

    fix footer walppapper uri filter backend category by title logic request by front on monted template

--- core/filters.py
+++ core/filters.py
@@ -4,7 +4,8 @@ from .models import Item
 
 class ItemFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
+    category__title = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Item
-        fields = ['title', 'category']
+        fields = ['title', 'category__title']
